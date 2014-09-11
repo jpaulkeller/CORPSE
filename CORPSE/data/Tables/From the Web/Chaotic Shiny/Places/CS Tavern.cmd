81303c6146f3a2b5e82f3a02713a7f93551456c2
jpaulkeller <jpaulkeller@b32450cf-0aee-e111-8f65-00e0815b4126>
2014-09-11 21:28:26 +0000

    CORPSE

--- CORPSE/data/Tables/From the Web/Chaotic Shiny/Places/CS Tavern.cmd
+++ CORPSE/data/Tables/From the Web/Chaotic Shiny/Places/CS Tavern.cmd
@@ -1,5 +1,108 @@
 / http://chaoticshiny.com/taverngen.php
 
+Tavern Tables
+a series of Tables for the Rolling of Dice to determine the Characterstics of Eating and Drinking Establishments and the Patrons therein
+
+By Hannah “Swordgleam” Lipsky http://chaoticshiny.com http://chaoticshinyproductions.com
+
+Roll a d100 on the adjective and noun tables to find a name for your tavern. Roll 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 Adjective _ Dancing Prancing Singing Laughing Drunken Cheerful Angry Lusty Happy Lonely Bragging Jumping Feisty Noble Golden Silver Shining Sterling Cowardly Brave Gambling Legendary Cursed Fighting Dreaming Sleeping Watchful Lucky Unlucky Frightful Naked Wandering 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50 51 52 53 54 55 56 57 58 59 60 61 62 63 64 65 66 Painted Boasting Screaming Laughing Dancing Crying Leaping Wretched Howling Thirsty Hungry Charming Mournful Vulgar Rude Hasty Darling Tired Smiling Yellow Violet Crimson Blue Friendly Bronze Brassy Brass Iron Hearty Timid Tricky Poor Nimble Vanishing 67 68 69 70 71 72 73 74 75 76 77 78 79 80 81 82 83 84 85 86 87 88 89 90 91 92 93 94 95 96 97 98 99 100 Dusty Flighty Naked Secret Scarlet Sweet Sour Kindly Heroic Enchanted Wishing Greedy Sleepy Daring Brawling Brawny Precious Cavorting Grinning Ugly Cheating False Honest Fiery Icy Cackling Coughing Flying Fat Skinny Sick Royal Limping Wet
+
+2
+
+Roll 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32
+
+Noun _ Wench Lass Lad Hostler Lord Knight Knave Juggler Minstrel Jester Stallion Mare Lady Zombie Bard Taylor Rogue Weasel Dragon Dog Hound Swordsman King Queen Sailor Jackal Slave Maid Kitten Falcon Giant Stableboy
+
+33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50 51 52 53 54 55 56 57 58 59 60 61 62 63 64 65 66
+
+Unicorn Wyvern Griffen Phoenix Priest Centaur Ghost Soldier Ghoul Priest Wizard Enchantress Harlot Witch Warlock Harpy Fox Troll Farmer Lion Snake Squirrel Badger Thief Cat Sparrow Lizard Ogre Blade Dagger Archer Star Moon Flame
+
+67 68 69 70 71 72 73 74 75 76 77 78 79 80 81 82 83 84 85 86 87 88 89 90 91 92 93 94 95 96 97 98 99 100
+
+Coin Mug Lover Goblin Foal Fighter Elf Bones Skeleton Skull Watchman Pirate Lamb Hunter Peasant Wolf Drake Duke Duchess Serpent Scallywag Robber Brigand Bandit Baron Player Gypsy Trader Tyrant Crow Raven Assassin Killer Merchant
+
+3
+
+Tavern Features
+Roll a d6 on each of the following tables to describe the tavern. Or, toss a handful of d6s on the page, and each one applies to the table it lands nearest to. If more than one die lands on the same table, they either apply to different features, or something very interesting is going on. If no dice land on a table, roll for it separately, or make it up. 1 2 3 4 5 6 Rotten Poor Fair Good Great Fit for royalty
+
+Quality
+
+1 2 3 4 5 6
+
+Cleanliness
+
+Filthy Grungy Tolerable Decent Unexpected Sparkling
+
+1 2 3 4 5 6
+
+Hole in the wall Tiny Cramped Typical Spacious Huge
+
+Size
+
+Anything not covered by another table. Roll once each for food, drinks, rooms, and service, or once for overall. 1 2 3 4 5 6 Rock bottom Cheap Affordable Pricy Extravagant Absurd
+
+How clean the floor, tables, plates, and mugs are. Roll for each, once overall, or just use the number from Quality. 1 2 3 4 5 6 Almost none Limited Expected Unusual Exotic Nigh unlimited
+
+Roll for the size of the tavern, of any rooms, and, if applicable, any booths, stages, or other features. 1 2 3 4 5 6 Lethal Nauseating Uninteresting Unusual Delicious Addictive
+
+Prices
+
+Variety
+
+Taste
+
+Roll once each for food, drink, available rooms, and any other goods or services. 1 2 3 4 5 6 Uninhabited Few patrons Some patrons Healthy crowd Sizeable crowd Spilling out the windows
+
+Roll once each for food, drinks, and any other goods or services on offer. 1 2 3 4 5 6 As the grave Whispers Subdued Conversational Shouting only Raucous cacophony
+
+Roll for food and drinks. This may or may not be the same as quality. 1 2 3 4 5 6 None – tavern well lit None occupied One occupied Half occupied All but one occupied All occupied
+
+Popularity
+
+Noise Level
+
+Dark Corners
+
+How many patrons are in the tavern at the time. Can also be used for room occupancy.
+
+How loud the patrons are. Can also be used to determine how quiet the rooms are.
+
+Can also be used as a measure of the overall shadiness of the tavern.
+
+4
+
+Notable Patrons
+Roll on any number of charts to describe a given patron, or toss the dice as described on the previous page. 1 2 3 4 5 6 1 2 3. 4 5 6 1 2 3 4 5 6 1 2 3 4 5 6 1 2 3 4 5 6 Half-naked Heavily cloaked Oddly dressed Expensively dressed Roughly dressed Unremarkable clothing Scrawny Willowy Slender Solid Muscular Heavy Flirty Cheerful Melancholy Worried Angry Smug
+
+Clothing
+
+1 2 3 4 5 6 1 2 3 4 5 6 1 2 3 4 5 6 1 2 3 4 5 6 1 2 3 4 5 6
+
+Heavily scarred Heavily tattooed Mildly scarred Some tattoos Healing wounds No markings Black Brown Red Blond(e) Unusual color Roll twice; mix of both Dead sober A few drinks Mildly tipsy Drunk A few too many drinks Near liver failure Friendly Neutral Cautious Curious Scornful Hostile
+
+Markings
+
+1 2 3 4 5 6 1 2 3 4 5 6 1 2 3 4 5 6 1 2 3 4 5 6 1 2 3 4 5 6
+
+One or more piercings One or more bracelets One or more rings One or more necklaces Unusual jewelry No jewelry Blue Green Brown Hazel Rare (grey, yellow, etc) Inhuman color Gambling Talking Drinking/buying drinks Dancing Shouting or singing None of the above Rough Husky Trained Lilting Melodious Heavily accented Card-sharking or tricks Magic tricks or cantrips Appraising others Brawling Knowledge/trivia Wilderness skills
+
+Jewelry
+
+Build
+
+Hair
+
+Eyes
+
+Apparent Mood
+
+Sobriety
+
+Current Activity
+
+
+
 Tavern/Inn Generator v0.8.6   Name: The Iron Rapier Inn  Find a new Tavern
 Overall Quality: Decent Cleanliness: Above average Size: Medium
 
@@ -37,6 +140,8 @@ Food Pricing: Low
 Room Pricing: Overpriced   
 Drink Quality: Very good
 Food Quality: Good
+
+
 
 Room Size: Small and medium   
 Drink Variety: Somewhat limited
