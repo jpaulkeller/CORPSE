027d5a3ff2a8440e39620f06fb4432b461a69673
jpaulkeller <jpaulkeller@b32450cf-0aee-e111-8f65-00e0815b4126>
2014-09-11 21:32:11 +0000

    CORPSE

---
+++ CORPSE/data/Integrated/Chaotic Shiny/CS Bazaar.cmd
@@ -0,0 +1,91 @@
+/ http://chaoticshiny.com/bazaargen.php
+
+__ __ __ __ __ __ __ 1
+__ 2  __ F1 __ __ __ __
+__ __ __ __ __ __ 3  4
+__ 5  __ __ 6  __ __ __
+
+Traffic: somewhat sparse
+Guards: very many
+Pickpockets: many
+Beggars: many
+
+Feature 1: inscribed monument
+
+Stall 1:  fresh meat and cold food
+    Quality: very low         Price: very low         Variety: above average 
+    Haggling: is enthusiastic         Owner: female, angry, surprisingly taciturn
+    Stall Type: wheelbarrow and some baskets
+
+Stall 2:  rations
+    Quality: below average         Price: below average         Variety: above average 
+    Haggling: is treated as an insult         Owner: male, spiteful, unusual hairstyle and a male, cynical, uses strange slang
+    Stall Type: semi-permanent makeshift structure
+
+Stall 3:  charms, enchanted items and enchantments
+    Quality: very high         Price: very high         Variety: average 
+    Haggling: only for the cheap items         Owner: female, nervous, paces
+    Stall Type: large cart which opens to display wares
+
+Stall 4:  gauntlets, cold food and hot drinks
+    Quality: below average         Price: below average         Variety: average 
+    Haggling: is enthusiastic         Owner: female, angry, paces
+    Stall Type: large cart which opens to display wares
+
+Stall 5:  ceremonial armor and greaves
+    Quality: above average         Price: above average         Variety: very low 
+    Haggling: occasionally involves barter         Owner: male, irritable, speaks very formally and a female, nervous, very flirtatious
+    Stall Type: large cart with goods piled haphazardly
+
+Stall 6:  spell wands
+    Quality: average         Price: above average         Variety: below average 
+    Haggling: is expected         Owner: female, spiteful, scatterbrained and a female, cynical, scatterbrained
+    Stall Type: several carts full of wares
+
+
+__ __ __ 1  __ 2  __ __ __ __
+__ __ __ 3  F1 __ __ __ 4  __
+5  __ __ __ 6  7  __ __ __ __
+
+Traffic: below average
+Guards: a large number
+Pickpockets: none
+Beggars: many
+
+Feature 1: large fountain
+Stall 1:  sweets and nuts
+    Quality: average         Price: average         Variety: average 
+    Haggling: is only allowed for certain items         Owner: female, mournful, unusual hair color
+    Stall Type: roofed and walled kiosk
+
+Stall 2:  animal toys
+    Quality: very high         Price: average         Variety: above average 
+    Haggling: is discouraged         Owner: male, spiteful, very overweight
+    Stall Type: large cart which opens to display wares
+
+Stall 3:  staves and axes
+    Quality: above average         Price: average         Variety: very high 
+    Haggling: is expected         Owner: male, spiteful, scatterbrained and a male, exuberant, limps
+    Stall Type: makeshift structure constructed each day
+
+Stall 4:  rations
+    Quality: average         Price: average         Variety: average 
+    Haggling: only for the cheap items         Owner: male, optimistic, twitches
+    Stall Type: tables and wheelbarrows
+
+Stall 5:  gauntlets
+    Quality: below average         Price: average         Variety: average 
+    Haggling: is expected         Owner: female, ill, speaks slowly
+    Stall Type: semi-permanent makeshift structure
+
+Stall 6:  fish and soup
+    Quality: very high         Price: very high         Variety: very low 
+    Haggling: is enthusiastic         Owner: female, angry, very short
+    Stall Type: large cart which opens to display wares
+
+Stall 7:  ceremonial weapons and staves
+    Quality: very low         Price: very low         Variety: very low 
+    Haggling: is expected         Owner: female, elderly, oddly pale and a female, bored, gangly
+    Stall Type: small table with goods piled haphazardly
+
+    
--- CORPSE/data/Scripts/Family.cmd
+++
@@ -1,4 +0,0 @@
-* Brief bio of a person's parents
-
-# {PC={Character Name?{!Name}}}
-{PC}: {Job}  Mother: {Job}  Father: {Job}
