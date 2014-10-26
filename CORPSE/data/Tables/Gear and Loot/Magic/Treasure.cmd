093683cbb93ba336c3d6b2e82aa96cf44cf61ce9
jpaulkeller <jpaulkeller@b32450cf-0aee-e111-8f65-00e0815b4126>
2014-10-26 15:24:35 +0000

    CORPSE

--- CORPSE/data/Tables/Gear and Loot/Magic/Treasure.cmd
+++ CORPSE/data/Tables/Gear and Loot/Magic/Treasure.cmd
@@ -2,3 +2,6 @@
 > Gems consist of: rubies ({0-10}), emeralds ({0-10}), sapphires ({0-10})
 >                  diamonds ({0-10}), pearls ({0-10}), opals ({0-10})
 > There is also a {Container} containing some {Mineral}
+
+{treasure}{5%? (the {inscription|design|decoration} provides a clue to its use)}
+ 
