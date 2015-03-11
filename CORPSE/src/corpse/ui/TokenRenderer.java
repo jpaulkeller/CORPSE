867a7694c98bc7e07d6c8d7e2cad19e1c78b094a
jpaulkeller <jpaulkeller@b32450cf-0aee-e111-8f65-00e0815b4126>
2015-03-11 02:59:51 +0000

    CORPSE

--- CORPSE/src/corpse/ui/TokenRenderer.java
+++ CORPSE/src/corpse/ui/TokenRenderer.java
@@ -4,33 +4,111 @@ import gui.db.ColoredTableRenderer;
 
 import java.awt.Color;
 import java.awt.Component;
+import java.awt.Point;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import javax.swing.BorderFactory;
 import javax.swing.JComponent;
+import javax.swing.JLabel;
 import javax.swing.JTable;
+
+import org.jdesktop.swingx.JXTable;
 
 /** This will highlight unresolved tokens. */
 
-public class TokenRenderer extends ColoredTableRenderer
+public class TokenRenderer extends ColoredTableRenderer implements MouseListener, MouseMotionListener
 {
    private static final long serialVersionUID = 1;
 
-   public static final String INVALID_OPEN = "<";
-   public static final String INVALID_CLOSE = ">";
+   public static final String INVALID_OPEN = "|";
+   public static final String INVALID_CLOSE = "|";
 
    // match unresolved tokens (but ignore the <!> last-match pattern)
-   private static final Pattern ERROR = Pattern.compile(INVALID_OPEN + "([^!]+)" + INVALID_CLOSE);
+   private static final String ERROR_REGEX = 
+      Pattern.quote(INVALID_OPEN) + "([^!]+)" + Pattern.quote(INVALID_CLOSE);
+   private static final Pattern ERROR = Pattern.compile(ERROR_REGEX);
+
+   private static final Pattern LINK = Pattern.compile("(.*)<(.+)>(.*)");
+
+   private int mouseRow, mouseCol;
+   private String link;
 
    @Override
-   public Component getTableCellRendererComponent(final JTable table, final Object value, final boolean isSelected,
-                                                  final boolean hasFocus, final int row, final int viewColumn)
+   public Component getTableCellRendererComponent(final JTable table, final Object value, 
+                                                  final boolean isSelected, final boolean hasFocus, 
+                                                  final int row, final int viewColumn)
    {
       JComponent cell = (JComponent) super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, viewColumn);
 
+      Matcher m = LINK.matcher(value.toString()); 
+      if (m.find())
+      {
+         JLabel label = (JLabel) cell;
+         String linkColor = isSelected ? "yellow" : "blue";
+         String html;
+         
+         if (mouseRow == row && mouseCol == viewColumn)
+         {
+            link = m.group(2);
+            html = "<font color=\"" + linkColor + "\"><u>" + link + "</u></font>";
+         }
+         else
+         {
+            link = null;
+            html = "<font color=\"" + linkColor + "\">" + m.group(2) + "</font>";
+         }
+         label.setText("<html>" + m.group(1) + html + m.group(3) + "</html>");
+      }
+
       if (ERROR.matcher(value.toString()).find())
          cell.setBorder(BorderFactory.createLineBorder(Color.RED, 2));
-
+      
       return cell;
+   }
+   
+   @Override
+   public void mouseClicked(MouseEvent e)
+   {
+      if (link != null)
+         System.out.println("TokenRenderer.mouseClicked(): " + link); // TODO
+   }
+
+   @Override
+   public void mouseMoved(MouseEvent e)
+   {
+      JXTable table = (JXTable) e.getSource();
+      Point p = e.getPoint();
+      mouseRow = table.rowAtPoint(p);
+      mouseCol = table.columnAtPoint(p);
+      table.repaint();
+   }
+   
+   @Override
+   public void mousePressed(MouseEvent e)
+   {
+   }
+
+   @Override
+   public void mouseReleased(MouseEvent e)
+   {
+   }
+
+   @Override
+   public void mouseEntered(MouseEvent e)
+   {
+   }
+
+   @Override
+   public void mouseExited(MouseEvent e)
+   {
+   }
+
+   @Override
+   public void mouseDragged(MouseEvent e)
+   {
    }
 }
