5be80fd1a86b39dff3b2a4c728a9dfeae9c3e5f3
jpaulkeller <jpaulkeller@b32450cf-0aee-e111-8f65-00e0815b4126>
2015-03-12 03:58:58 +0000

    CORPSE

---
+++ CORPSE/data/Tables/People/Background/Background.cmd
@@ -0,0 +1,35 @@
+/ http://random-generator.com/index.php?title=Fantasy_Character_History
+
+<html>
+<body>
+
+/ Query for name (using random default); assign and randomize
+# {PC={Name?{Name}}}
+
+<h1>{PC}</h1>
+
+<ul>
+  <li><b>Character Story</b>
+  <ul>
+    <li>Legitimacy: [LEGITIMACY]
+    <li>Family Head: [FAMILY_HEAD]
+    <li>FH's Occupation: [OCCUPATION]
+  </ul>
+  
+  <li><b>Birth Circumstances</b>
+  <ul>
+    <li>Siblings: [NUMBER_SIBLINGS]<br/>
+    <li>Place of Birth: [Fantasy Birth.PLACE_OF_BIRTH]<br/>
+    <li>Unusual Birth: [Fantasy Birth.UNUSUAL_BIRTH]<br/>
+  </ul>
+
+  <li><b>Life Path</b>
+  <ul>
+    <li>===Childhood Events===[Fantasy Life Events.EVENTS_OF_YOUTH]<br/>
+    <li>===Adolescent Events===[Fantasy Life Events.EVENTS_OF_YOUTH]<br/>
+    <li>===Adult Events===[Fantasy Life Events.EVENTS_OF_ADULTHOOD]<br/>
+  </ul>
+</ul>
+
+</body>
+</html>
