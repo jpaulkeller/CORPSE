b46a9214b74ca8160b881796c29dd6695052abe0
jpaulkeller <jpaulkeller@b32450cf-0aee-e111-8f65-00e0815b4126>
2015-06-15 22:32:35 +0000

    CORPSE

---
+++ CORPSE/data/Tables/People/Misc/Cult.cmd
@@ -0,0 +1,22 @@
+Age
+Founder(s)
+Funding
+Goal
+Historical Events
+Initiation Ceremony
+Know Allies/Enemies
+Leader
+Location
+Members
+Practices – Customs
+Practices – Restrictions
+Practices – Rites & Rituals
+Practices – Sacrifices
+Secrecy Level
+Symbol
+Time period
+Tools & Artifacts - Artifacts
+Tools & Artifacts – Tools/Items
+Tools & Artifacts - Weapons
+
+===
