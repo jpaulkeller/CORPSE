5801313f271b82d28b3154155851f5d731cdc5de
jpaulkeller <jpaulkeller@gmail.com>
2015-12-16 17:11:21 +0000

    CORPSE

--- CORPSE/data/Tables/Gear and Loot/Loot/Treasure.cmd
+++ CORPSE/data/Tables/Gear and Loot/Loot/Treasure.cmd
@@ -1,88 +1,44 @@
 / http://random-generator.com/index.php?title=Interesting_Treasure_Hoard_Generator
+/ http://www.mithrilandmages.com/utilities/1ETreasure.php
+/ http://angband.oook.cz/steamband/Treasure.pdf
+/ http://www.giantitp.com/forums/showthread.php?453619-Art-Objects-Pimp-your-BBEG-s-crib!
+/ https://rpgathenaeum.wordpress.com/tag/pimp-my-treasure-parcel/
+/ https://www.goodreads.com/author_blog_posts/871940-precious-things-japanese-art-objects-for-your-d-d-game
 
 <html>
 <body>
 
 / Query for seed (using random default); assign and randomize
 # {T={Treasure?{1000-9999}}}
+/ {S:=Loot}
+/ {L:=Loot}
+{S={Size (1-5)?{5}}}
+{L={Level (1-9)?{9}}}
+{R={2*{{S}*{L}}}}
+{Q={L*4}}
 
-<h1>Treasure: {T}</h1>
+<h1>Treasure #{T} (size: {S}, level: {L}, rank: {R}) </h1>
 
 <ul>
-  <li><b>What:</b> 
-      <ul>
-        <li>
-      </ul>
-      
-  <li><b>Why</b>
-      <ul>
-        <li>
-      </ul>
+!loop {S}
+   <li>{container:small loot} holding {N={{S}d20}}{C:=currency} {N} {C.nickname}s ({C.coin}s, worth about {{N}*{C.cp}} cp)
+   {{R}% <li>{container:small loot} holding {{S}d20} coins: {coin}}
+   {{R}% <li>{container:small loot} holding {{S}d{L}} {{~{gem cost.gem}}+}{50% and {{S}d{L}} {{~{gem cost.gem{!different}}}+}}}
+   {{R}% <li>{container:medium loot} holding {{S}d4} {metal} {ingots|bars}}
+   {{R}% <li>Luxury: {LUXURY:ITEM} (worth {{L}d25}0 gp)}
+   {{R}% <li>Art: {ART}{50% (by {ART:ARTIST})}}
+   {{R}% <li>Jewelry: {luxury:jewelry} (worth {{L}d25}0 gp)}
+   {{R}% <li>Magic Item: {ENCHANTED ITEM}}
+!loop end
+   {Q}% <li>Artifact: {ARTIFACT NAME}}
+   {Q}% <li>Relic: {RELIC}}
 </ul>
+
+/   <li>Hidden: {treasure:hidden}
+/   <li>{treasure}{5% (the {inscription|design|decoration} provides a clue to its use)}
+/   <li>Condition: {condition:any}
+/   <li>Quality: {quality:any}
+/ {:misc}
 
 </body>
 </html>
-
-> Coins consist of: {200} gold, {500} silver, {1000} copper
-> Gems consist of: rubies ({0-10}), emeralds ({0-10}), sapphires ({0-10})
->                  diamonds ({0-10}), pearls ({0-10}), opals ({0-10})
-> There is also a {Container} containing some {Mineral}
-
-{treasure}{5% (the {inscription|design|decoration} provides a clue to its use)}
-
-====
-{Unguarded 1}{50%, {Unguarded 1}{10%, {Unguarded 1}{5%, {Unguarded 1}{1%, {Unguarded 1}}}}}
- 
-== 1st Dungeon Level ==
-
-=== Unguarded Treasure ===
-<sgdisplay iterations = "2">You find [un1stlevel]<br/></sgdisplay>
-
-=== Individual Treasure ===
-<sgdisplay iterations = "4">You find [indv1st]</sgdisplay>
-
-=== Small Hoard Treasure ===
-<sgdisplay iterations = "1">You find [sm1stlevel]<br/></sgdisplay>
-
-=== Medium Hoard Treasure ===
-<sgdisplay iterations = "1">You find [med1stlevel]<br/></sgdisplay>
-
-=== Large Hoard Treasure ===
-<sgdisplay iterations = "1">You find [lg1stlevel]<br/></sgdisplay>
-
-
-== 2nd Dungeon Level ==
-
-=== Unguarded Treasure ===
-<sgdisplay iterations = "2">You find [un2ndlevel]<br/></sgdisplay>
-
-=== Individual Treasure ===
-<sgdisplay iterations = "4">You find [indv2nd]</sgdisplay>
-
-=== Small Hoard Treasure ===
-<sgdisplay iterations = "1">You find [sm2ndlevel]<br/></sgdisplay>
-
-=== Medium Hoard Treasure ===
-<sgdisplay iterations = "1">You find [med2ndlevel]<br/></sgdisplay>
-
-=== Large Hoard Treasure ===
-<sgdisplay iterations = "1">You find [lg2ndlevel]<br/></sgdisplay>
-
-
-== 3rd Dungeon Level ==
-
-=== Unguarded Treasure ===
-<sgdisplay iterations = "2">You find [un3rdlevel]<br/></sgdisplay>
-
-=== Individual Treasure ===
-<sgdisplay iterations = "4">You find [indv3rd]</sgdisplay>
-
-=== Small Hoard Treasure ===
-<sgdisplay iterations = "1">You find [sm3rdlevel]<br/></sgdisplay>
-
-=== Medium Hoard Treasure ===
-<sgdisplay iterations = "1">You find [med3rdlevel]<br/></sgdisplay>
-
-=== Large Hoard Treasure ===
-<sgdisplay iterations = "1">You find [lg3rdlevel]<br/></sgdisplay>
- 
