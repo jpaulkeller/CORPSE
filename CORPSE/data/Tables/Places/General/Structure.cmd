dd16631c4663b42f3076213dd5cc9bee20399b84
jpaulkeller <jpaulkeller@gmail.com>
2016-04-06 16:22:50 +0000

    CORPSE

---
+++ CORPSE/data/Tables/Places/General/Structure.cmd
@@ -0,0 +1,21 @@
+<html>
+<body>
+
+/ http://theaussiegm.tumblr.com/post/140255704743/after-a-suggestion-from-scotaire-heres-my
+/ https://drive.google.com/file/d/0BzFqRLlZdgtjRWxlYUV2cDZYMk0/view
+
+/ Query for name (using random default); assign and randomize
+# {N={Structure?{structure}}}
+
+<h1>{N}</h1>
+
+<ul>
+  <li><b>Description:</b> {site:adj}</li>
+  <li><b>Of Note:</b> {structure flavor}</li>
+  <li><b>Construction:</b> {building material}</li>
+  <li><b>Condition:</b> {site:condition}{20%, now {{site:abandoned}|ruins, destroyed by {disaster}}}</li>
+  <li><b>Occupants:</b> {none|{d4+1} people|{d6+3} people}</li>
+</ul>
+
+</body>
+</html>
