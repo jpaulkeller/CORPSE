c02b6d6d8f78006786273f9111d3dbf108484c1f
jpaulkeller <jpaulkeller@b32450cf-0aee-e111-8f65-00e0815b4126>
2016-04-06 16:22:50 +0000

    CORPSE

---
+++ CORPSE/data/Tables/World/World Overview.cmd
@@ -0,0 +1,46 @@
+/ http://toastyhat.tumblr.com/post/119298071024/35-world-building-questions
+
+<html>
+<body>
+
+/ Query for name (using random default); assign and randomize
+# {NAME={Name?{Generated Name}}}
+
+<h1>{NAME}</h1>
+
+<ul>
+  <li>General Description: 
+  <li>What are the geography and climate of the area like?
+  <li>What kinds of animals inhabit the area?
+  <li>Why is this city placed where it is?
+  <li>What is the cuisine like?
+  <li>What are the major religions and how is worship expressed?
+  <li>What are some local folk tales/legends?
+  <li>What are the major holidays?
+  <li>What languages are spoken in the area?
+  <li>Are there variations in the main language–accents or dialects?
+  <li>Are there any widely-used slang words or figures of speech?
+  <li>How far has technology advanced?
+  <li>What is the government structure?
+  <li>What are the area’s major exports/products?
+  <li>How is education carried out?
+  <li>What is the situation/treatment of the lower class like?
+  <li>What are some notable laws?
+  <li>What are the customs and rules of etiquette?
+  <li>What are the most readily available careers?
+  <li>What do people do for entertainment/recreation?
+  <li>What does the architecture look like?
+  <li>What are some significant landmarks?
+  <li>How are people dressed?
+  <li>What is the political climate?
+  <li>Is there a war ongoing or upcoming?
+  <li>How large is the population?
+  <li>What are the major gender norms?
+  <li>General views on sexuality?
+  <li>How do societal expectations change with a person’s age?
+  <li>What does the average household look like?
+  <li>What are the standards of beauty?
+</ul>
+
+</body>
+</html>
